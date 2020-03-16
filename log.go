8d5a3413f153ab839e3f5521e037f08239a5bcaf
Matthew Wilkinson <scfix@outlook.com>
2020-03-16 17:09:06 -0400

    Fixes in styling for the go code

--- log.go
+++ log.go
@@ -19,55 +19,77 @@ type logevt struct {
 	Timestamp  int64      `xml:"timestamp,attr"`
 	Thread     int        `xml:"thread,attr"`
 	Message    string     `xml:"log4j:message"`
-	Properties []property `xml:"log4j:properties>log4j:data"`
+	Properties []property `xml:"log4j:properties>log4j:datat"`
 }
 
 var ch chan *logevt
+var loggerName string
+
+const log4JTag = "log4j:event"
+
+type logLevel string
+
+const (
+	// Trace low level rapid fire logs
+	Trace = "TRACE"
+	// Debug Programmer level info about specific components
+	Debug = "DEBUG"
+	// Info High Level info about specific components
+	Info = "INFO"
+	// Error problems in the code
+	Error = "ERROR"
+	// Fatal  oops
+	Fatal = "FATAL"
+)
 
 // Initialize inits the logging function
-func Initialize() {
+func Initialize(programName string) {
 	fmt.Println("Init log4go")
 	c := make(chan *logevt, 1000)
 	ch = c
-
+	loggerName = programName
 	go messagePump(ch)
+}
+
+func makeLogEvent(level logLevel, message string) *logevt {
+	return &logevt{
+		log4JTag,
+		loggerName,
+		string(level),
+		makeTimestamp(),
+		1,
+		message,
+		[]property{{"log4japp", "TBD"}, {"log4jmachinename", "Local Machine"}}, //TODO(MWILKINSON):  Determine the best way to get the app name and the Machine name
+	}
 }
 
 // LogTrace Logs Trace Messages
 func LogTrace(s string) {
-	var p = makeLogEvt()
-	p.Level = "Trace"
-	p.Message = s
+	var p = makeLogEvent(Trace, s)
 	ch <- p
 }
 
 // LogDebug Logs Debug Messages
 func LogDebug(s string) {
-	var p = makeLogEvt()
-	p.Level = "DEBUG"
-	p.Message = s
+	var p = makeLogEvent(Debug, s)
 	ch <- p
 }
 
 // LogInfo Logs Info Messages
 func LogInfo(s string) {
-	var p = makeLogEvt()
-	p.Level = "INFO"
-	p.Message = s
+	var p = makeLogEvent(Info, s)
 	ch <- p
 }
 
 // LogError Logs Error Messages
 func LogError(s string) {
-	var p = makeLogEvt()
-	p.Level = "ERROR"
-	p.Message = s
+	var p = makeLogEvent(Error, s)
 	ch <- p
 }
 
 // LogFatal Logs Fatal Messages
 func LogFatal(s string) {
-	var p = makeLogEvt()
+	var p = makeLogEvent(Fatal, s)
 	p.Level = "Fatal"
 	p.Message = s
 	ch <- p
@@ -75,19 +97,6 @@ func LogFatal(s string) {
 
 func makeTimestamp() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
-}
-
-func makeLogEvt() *logevt {
-	//NOTE(MWILKINSON): This method is hampered by the lack of reflection in this implementation. The goal here is to make this generic enough for any appliation to have relevant meta data
-	return &logevt{
-		"log4j:event",
-		"GetToWork",
-		"Trace",
-		makeTimestamp(),
-		1,
-		"",
-		[]property{{"log4japp", "TBD"}, {"log4jmachinename", "Local Machine"}}, //TODO(MWILKINSON):  Determine the best way to get the app name and the Machine name
-	}
 }
 
 func messagePump(ch chan *logevt) {
@@ -99,13 +108,11 @@ func messagePump(ch chan *logevt) {
 		}
 		for {
 			p := <-ch
-			//conn.Write([]byte(fmt.Sprintf("A sample Message %d", i)))
 			b, err := xml.Marshal(p)
 			if err != nil {
 				fmt.Println("An Error was encountered")
 			}
 			conn.Write(b)
-
 		}
 	}
 }
